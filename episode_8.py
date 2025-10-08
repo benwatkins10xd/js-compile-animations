e5243759e8202395d02e4b49373dff166c964ac0
Ben Watkins <benwatkins01@hotmail.com>
2025-10-08 12:45:20 +0100

    add more animation

--- episode_8.py
+++ episode_8.py
@@ -5,10 +5,11 @@ class LegacyParserScene(Scene):
     """Animation explaining how the current (legacy) parser parses."""
 
     def construct(self):
-        tokens_list = ["2", "^", "3", "*", "4"]
-
+        # start with the raw code here
         text = Code(code_string="2 ^ 3 * 4", add_line_numbers=False).to_edge(UP)
 
+        # lex the raw string
+        tokens_list = ["2", "^", "3", "*", "4"]
         tokens = VGroup(
             [Code(code_string=token, add_line_numbers=False) for token in tokens_list]
         ).arrange(RIGHT, buff=0.5)
@@ -17,6 +18,7 @@ class LegacyParserScene(Scene):
         self.wait(1)
         self.play(Transform(text, tokens.to_edge(UP)))
 
+        # these are our methods
         parse_method = Code(code_string="parse()", add_line_numbers=False)
         parse_binary_method = Code(
             code_string="parseBinaryExpression()", add_line_numbers=False
@@ -54,15 +56,79 @@ class LegacyParserScene(Scene):
         ).to_corner(DL)
         self.play(Write(parent_priority_tracker))
 
-        # parseBinaryExpression calls parsePrimaryExpression, which simply returns a number token
+        # parseBinaryExpression calls parsePrimaryExpression
         self.play(Indicate(methods[0], color=BLUE))
         method_pointer.next_to(methods[0], RIGHT, buff=0.2)
-        token_circle = Circle(radius=0.4, color=BLUE).to_edge(RIGHT, buff=0.5)
+        two_token_circle = Circle(radius=0.4, color=BLUE).to_edge(RIGHT, buff=3)
         token_number = MathTex("2", color=WHITE)
-        token_number.move_to(token_circle.get_center())
-        self.play(Write(token_circle))
+        token_number.move_to(two_token_circle.get_center())
+        self.play(Write(two_token_circle))
         self.play(Write(token_number))
+
+        # it returns a number token and advances to the next token
+        token_pointer.next_to(tokens[1], DOWN, buff=0.2)
 
         # then we enter the while loop in the parseBinaryExpression function
         self.play(Indicate(methods[1], color=BLUE))
         method_pointer.next_to(methods[1], RIGHT, buff=0.2)
+
+        # current token is ^ (caret), in the while loop we get the operator priority for this which is 3
+        token_priority = Code(
+            code_string="priority = 3", add_line_numbers=False
+        ).next_to(parent_priority_tracker, RIGHT, buff=0.2)
+        self.play(Write(token_priority))
+
+        # is priority === 0 or < parentPriority? both are false, so continue by consuming the caret token
+        caret_token_circle = Circle(radius=0.4, color=BLUE).to_edge(UR, buff=2)
+        token_number = MathTex("\mathbin{\char`\^}", color=WHITE)
+        token_number.move_to(caret_token_circle.get_center())
+        self.play(Write(caret_token_circle))
+        self.play(Write(token_number))
+        token_pointer.next_to(tokens[2], DOWN, buff=0.2)
+
+        # now, recurse by calling parseBinaryExpression with parentPriority = 3
+        self.play(Indicate(methods[1], color=BLUE))
+        token_priority.remove()
+        new_priority = Code(
+            code_string="parentPriority = 3", add_line_numbers=False
+        ).to_corner(DL)
+        self.play(ReplacementTransform(parent_priority_tracker, new_priority))
+        parent_priority_tracker = new_priority
+
+        # parseBinaryExpression calls parsePrimaryExpression again
+        self.play(Indicate(methods[0], color=BLUE))
+        method_pointer.next_to(methods[0], RIGHT, buff=0.2)
+        three_token_circle = Circle(radius=0.4, color=BLUE).to_edge(RIGHT, buff=1)
+        token_number = MathTex("3", color=WHITE)
+        token_number.move_to(three_token_circle.get_center())
+        self.play(Write(three_token_circle))
+        self.play(Write(token_number))
+
+        # it returns a number token and advances to the next token
+        token_pointer.next_to(tokens[3], DOWN, buff=0.2)
+
+        # enter the while loop, inside the recursive call
+        self.play(Indicate(methods[1], color=BLUE))
+        method_pointer.next_to(methods[1], RIGHT, buff=0.2)
+
+        # current token is *, get the priority which is 2
+        token_priority = Code(
+            code_string="priority = 2", add_line_numbers=False
+        ).next_to(parent_priority_tracker, RIGHT, buff=0.2)
+        self.play(Write(token_priority))
+
+        # 2 is less than or equal to the parentPriority, 3, so break and return the number token
+        # we are now back in the original call. finally for this iteration, we create a new
+        # BinaryExpression and set it to our LHS.
+        self.play(Indicate(methods[1], color=BLUE))
+        # caret_token_circle.add_line_to(two_token_circle)
+        # caret_token_circle.add_line_to(three_token_circle)
+
+        # now we're back in the while loop, with the current token being *
+        # get the priority of it
+        token_priority = Code(
+            code_string="priority = 2", add_line_numbers=False
+        ).next_to(parent_priority_tracker, RIGHT, buff=0.2)
+        self.play(Write(token_priority))
+
+        # check priority is
